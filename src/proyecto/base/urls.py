4f554ada8a91c14e13b94d5457a780a3c6e84bdc
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-23 17:58:51 -0600

    Se agrega formulario para Nueva Tarea

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ListaPendientes, DetalleTarea, CrearTarea
 
-urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'), # as_view() hace una lista de clases a una lista normal
+urlpatterns = [path('', ListaPendientes.as_view(), name='tareas'), # as_view() hace una lista de clases a una lista normal
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
                path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea')] 
