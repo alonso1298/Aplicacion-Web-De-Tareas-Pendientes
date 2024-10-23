7c10bc3fe85723b5804b22770ae04de0c930f974
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-22 20:34:16 -0600

    Se configura la vista de detalle

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import ListaPendientes
+from .views import ListaPendientes, DetalleTarea
 
-urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes')] # as_view() hace una lista de clases a una lista normal
+urlpatterns = [path('', ListaPendientes.as_view(), name='pendientes'),
+               path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea')] # as_view() hace una lista de clases a una lista normal
--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Tarea
 
 class ListaPendientes(ListView):
     model = Tarea
-    context_object_name = 'tareas'
+    context_object_name = 'tareas' #Este es el alias de la clase
+
+class DetalleTarea(DetailView):
+    model = Tarea
+    context_object_name = 'tarea'
+    template_name = 'base/tarea.html'
