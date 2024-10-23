5a3085b602b0d33595f2204c3c211cef436ee228
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-22 19:36:49 -0600

    Se crea la tabla de tareas

--- src/proyecto/base/models.py
+++ src/proyecto/base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tarea(models.Model):
+    usuario = models.ForeignKey(User,
+                                on_delete=models.CASCADE, #CASCADE indica que si el usuario es eliminado, elimina cualquier tarea de este mismo
+                                null=True,
+                                blank=True)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True,
+                                   blank=True)
+    completo = models.BooleanField(default=False)
+    creado = models.DateField(auto_now_add=True) #Se genera automaticamente la hora y la fecha de creación
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        #Las tareas que estan compretas se van al fondo de la lista
+        ordering = ['completo'] 
