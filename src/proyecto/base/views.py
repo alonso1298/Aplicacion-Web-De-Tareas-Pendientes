5028c8bbe27d4fceba42d886e82f596b584c0a4e
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-23 19:46:00 -0600

    Se agrega formulario de logueo / deslogueo

--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy # Te lleva a una URL automaticamente ante la ocurrencia de un evento
 from .models import Tarea
 
+class Logueo(LoginView):
+    template_name = "base/login.html"
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tareas')
+
+@login_required
 class ListaPendientes(ListView):
     model = Tarea
     context_object_name = 'tareas' #Este es el alias de la clase
