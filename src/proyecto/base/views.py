b95e0efe1c2782afdbbf79b7af5a376a14c9b21e
alonso1298 <alonsosagrerogranados@gmail.com>
2024-10-23 18:20:04 -0600

    Se agrega la clase para eliminar tareas

--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy # Te lleva a una URL automaticamente ante la ocurrencia de un evento
 from .models import Tarea
 
@@ -24,3 +24,8 @@ class EditarTarea(UpdateView):
     model = Tarea
     fields = '__all__' #Incrpora todos los campos del modelo
     success_url = reverse_lazy('tareas') #cargamos el sitio donde nos llevara automaticamente el sitio
+
+class EliminarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tarea' # Los elementos del objetos son referenciados como tareas
+    success_url = reverse_lazy('tareas')
